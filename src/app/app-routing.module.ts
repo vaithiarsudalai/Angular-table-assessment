303b485329ba0bc2abdbe8c1216d0315e178d3e9
vaithiarsudalai <sudalaivaithiar@gmail.com>
2019-09-21 23:22:35 +0530

    Angular- table app assessment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
+import { AddtaskComponent } from './addtask/addtask.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'', component : AddtaskComponent},
+  {path :'view', component : ViewtaskComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
+import { AddtaskComponent } from './addtask/addtask.component';
+import { ViewtaskComponent } from './viewtask/viewtask.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtaskComponent,
+    NavbarComponent,
+    ViewtaskComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
